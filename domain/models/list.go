898a4b91b4352b13c07fdeb5647aa2c1a221bbe8
Svetlana Tsvetkova <lana.ts97@gmail.com>
2025-11-30 18:26:04 +0300

    [i] add validation to models

--- domain/models/list.go
+++ domain/models/list.go
@@ -1,8 +1,30 @@
 package models
 
+import "errors"
+
 type List struct {
 	Id      int64
 	Name    string
 	ChatId  int64
 	OwnerId int64
+}
+
+func (l *List) Validate() error {
+	if l.Id <= 0 {
+		return errors.New("List id should be positive")
+	}
+
+	if l.ChatId <= 0 {
+		return errors.New("List ChatId should be positive")
+	}
+
+	if l.OwnerId <= 0 {
+		return errors.New("List OwnerId should be positive")
+	}
+
+	if l.Name == "" {
+		return errors.New("List Name should not be null")
+	}
+
+	return nil
 }
