7d6932c0c3289c537266319e0802f6c11d8e9620
Svetlana Tsvetkova <lana.ts97@gmail.com>
2025-11-30 21:20:30 +0300

    buttons basr

--- api/handlers.go
+++ api/handlers.go
@@ -3,6 +3,7 @@ package api
 import (
 	"fmt"
 	"log/slog"
+	"strings"
 
 	"gopkg.in/telebot.v3"
 )
@@ -31,4 +32,69 @@ func (h *Handlers) StartHandler(c telebot.Context) error {
 	message := fmt.Sprintf("привет, %s", user.Username)
 
 	return c.Send(message)
+}
+
+func (h *Handlers) CreateListHandler(c telebot.Context) error {
+
+}
+
+func (h *Handlers) HelpHandler(c telebot.Context) error {
+	message := `🎮 *Управление ботом-органайзером* 🤪
+
+*Просто нажми на кнопку ↓*`
+
+	keyboard := &telebot.ReplyMarkup{}
+
+	btnCreate := keyboard.Data("🎉 Создать список", "btn_create")
+	btnShow := keyboard.Data("📜 Показать списки", "btn_lists")
+	btnAdd := keyboard.Data("➕ Добавить пункт", "btn_add")
+
+	btnDelete := keyboard.Data("🗑️ Удалить список", "btn_delete")
+	btnHelp := keyboard.Data("❓ Помощь", "btn_help")
+
+	keyboard.Inline(
+		keyboard.Row(btnCreate),
+		keyboard.Row(btnShow, btnAdd),
+		keyboard.Row(btnDelete, btnHelp),
+	)
+
+	return c.Send(message, &telebot.SendOptions{
+		ParseMode:   "Markdown",
+		ReplyMarkup: keyboard,
+	})
+}
+
+func (h *Handlers) CallbackHandler(c telebot.Context) error {
+	callback := c.Callback()
+
+	defer c.Respond()
+
+	cleanData := strings.TrimSpace(callback.Data)
+	cleanData = strings.ReplaceAll(cleanData, "\n", "")
+	cleanData = strings.ReplaceAll(cleanData, "\u000c", "")
+	cleanData = strings.TrimSpace(cleanData)
+
+	h.logger.Info("callback processed",
+		"original", fmt.Sprintf("%q", callback.Data),
+		"cleaned", cleanData)
+
+	switch cleanData {
+	case "btn_create":
+		return c.Send("🎯 *Создание нового списка!*\n\nКак назовем наш список безумия? 💥",
+			&telebot.SendOptions{ParseMode: "Markdown"})
+	case "btn_lists":
+		return c.Send("📋 *Вот твои списки!*\n\nСкоро здесь появятся твои списки...",
+			&telebot.SendOptions{ParseMode: "Markdown"})
+	case "btn_add":
+		return c.Send("➕ *Добавляем пункт!*\n\nКакой пункт добавляем в список? 📝",
+			&telebot.SendOptions{ParseMode: "Markdown"})
+	case "btn_delete":
+		return c.Send("🗑️ *Удаляем список!*\n\nКакой список отправляем в небытие? 💀",
+			&telebot.SendOptions{ParseMode: "Markdown"})
+	case "btn_help":
+		return h.HelpHandler(c)
+	default:
+		h.logger.Warn("unknown callback", "data", cleanData)
+		return c.Send("❌ Неизвестная команда")
+	}
 }
--- api/routes.go
+++ api/routes.go
@@ -8,12 +8,19 @@ type Route struct {
 	Name     string
 }
 
+// type RouteHandler func(c telebot.Context) error
+
 func (h *Handlers) GetRoutes() []Route {
 	return []Route{
 		{
 			Endpoint: "/start",
 			Handler:  h.StartHandler,
 			Name:     "start",
+		},
+		{
+			Endpoint: "/help",
+			Handler:  h.HelpHandler,
+			Name:     "help",
 		},
 	}
 }
@@ -27,4 +34,16 @@ func (h *Handlers) SetupHandlers(bot *telebot.Bot) {
 			"route", route.Name,
 			"endpoint", route.Endpoint)
 	}
+
+	//кнопки
+	bot.Handle(telebot.OnCallback, h.CallbackHandler)
 }
+
+// func Init() {
+
+// 	HandleRoute("start", "/start", h.StartHandler)
+// }
+
+// func HandleRoute(name, path string, handler RouteHandler) {
+
+// }
